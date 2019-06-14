438a6260bc9652fdae9cfb2beabd25d8f424a8d2
Asif67 <asif.nwu@gmail.com>
2019-06-15 01:48:21 +0600

    Added More Componets for Control

--- app/src/main/java/com/example/webbraincomputers/observingrobotcontrol/LineFollow.kt
+++ app/src/main/java/com/example/webbraincomputers/observingrobotcontrol/LineFollow.kt
@@ -2,11 +2,15 @@ package com.example.webbraincomputers.observingrobotcontrol
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_line_follow.*
+import kotlinx.android.synthetic.main.activity_obstacle_avoid.*
 
 class LineFollow : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_line_follow)
+
+        lfWebView.loadUrl("http://192.168.1.47:8000")
     }
 }
--- app/src/main/java/com/example/webbraincomputers/observingrobotcontrol/ManualControl.kt
+++ app/src/main/java/com/example/webbraincomputers/observingrobotcontrol/ManualControl.kt
@@ -2,11 +2,15 @@ package com.example.webbraincomputers.observingrobotcontrol
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_manual_control.*
+import kotlinx.android.synthetic.main.activity_obstacle_avoid.*
 
 class ManualControl : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_manual_control)
+
+        mcWebView.loadUrl("http://192.168.1.47:8000")
     }
 }
