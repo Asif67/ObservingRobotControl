3b19e1a8b5004cfd4815a0b5e7792da5e6285e0f
Asif67 <asif.nwu@gmail.com>
2019-06-14 15:25:37 +0600

    patch-1(Added WebView)

--- app/src/main/java/com/example/webbraincomputers/observingrobotcontrol/ObstacleAvoid.kt
+++ app/src/main/java/com/example/webbraincomputers/observingrobotcontrol/ObstacleAvoid.kt
@@ -2,11 +2,20 @@ package com.example.webbraincomputers.observingrobotcontrol
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
+import kotlinx.android.synthetic.main.activity_obstacle_avoid.*
+
 
 class ObstacleAvoid : AppCompatActivity() {
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_obstacle_avoid)
+
+        oaWebView.loadUrl("http://192.168.1.47:8000")
+
+
     }
 }
